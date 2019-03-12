959f3e1d61ece2f999f30323845585b79075e92f
Addama <addama.sanders@gmail.com>
2019-03-12 12:03:44 -0700

    Initial creation

---
+++ twitter_ux_bookmarklet_alternative.js
@@ -0,0 +1,1 @@
+javascript:(function(){var p=document.getElementsByTagName('main')[0].firstChild.children[1].firstChild.firstChild;p.nextSibling.remove();p.style.maxWidth='100%';p.style.margin='10px auto';p.children[0].lastElementChild.style.maxWidth='100%'})()
